91837e4a127c192a69975da227a2f11a5e8834f8
hassanas <you@example.com>
2021-06-23 18:30:35 +0500

    run magento cron file

---
+++ TestApp.php
@@ -0,0 +1,20 @@
+<?php
+class TestApp extends \Magento\Framework\App\Http implements \Magento\Framework\AppInterface
+{
+    public function launch()
+    {
+        //echo "Ali";exit;
+        /** @var \Vendor\Module\Cron\Test $cron */
+        $cron = \Magento\Framework\App\ObjectManager::getInstance()
+            ->create('Evx\SAPImport\Cron\ImportStocks');
+
+        $cron->execute();
+        return $this->_response;
+    }
+
+    public function catchException(\Magento\Framework\App\Bootstrap
+    $bootstrap, \Exception $exception) : bool
+    {
+        return false;
+    }
+}
---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+
+//echo "hassa";exit;
+require '../../app/bootstrap.php';
+require __DIR__ . '/TestApp.php';
+$bootstrap = \Magento\Framework\App\Bootstrap::create(BP, $_SERVER);
+
+$obj = $bootstrap->getObjectManager();
+$state = $obj->get('Magento\Framework\App\State');
+$state->setAreaCode('frontend');
+
+/** @var \Magento\Framework\App\Http $app */
+$app = $bootstrap->createApplication('TestApp');
+$bootstrap->run($app);
